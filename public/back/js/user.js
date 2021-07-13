f1b08c03d3dde1e16e09d0e2d646218f0b7ff4bf
ning01 <87949768@qq.com>
2021-07-13 14:28:26 +0800

    分页效果完成

--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,23 +1,62 @@
 $(function () {
-  
-  $.ajax({
-    type: "get",
-    url: "/user/queryUser",
-    data: {
-      page: 1,
-      pageSize:5
-    },
-    dataType: "json",
-    success: function (info) {
-      console.log(info);
-      //生成htmlstr,将来进行渲染
-      //参数1:模板id,参数2:数据对象
-      //在模板中,可以直接访问传进去对象中的所有属性
-      var htmlStr = template("tmp", info);
-      $("tbody").html(htmlStr);
+  var currentPage = 1;
+  var pageSize = 5;
+  render();
+
+  function render() {
       
+    $.ajax({
+      type: "get",
+      url: "/user/queryUser",
+      data: {
+        page: currentPage,
+        pageSize:pageSize
+      },
+      dataType: "json",
+      success: function (info) {
+        console.log(info);
+        //生成htmlstr,将来进行渲染
+        //参数1:模板id,参数2:数据对象
+        //在模板中,可以直接访问传进去对象中的所有属性
+        var htmlStr = template("tmp", info);
+        $("tbody").html(htmlStr);
+
+
+
+
+        
+        
+        //进行分页初始化
+        $("#paginator").bootstrapPaginator({
+          //版本号
+          bootstrapMajorVersion: 3,
+          //总页数
+          totalPages: Math.ceil( info.total / info.size),
+          //当前页
+          currentPage: info.page,
+          
+          //点击事件
+          onPageClicked: function (a, b, c, page) {
+            
+            
+            // console.log(page);
+            //根据page ,请求对应页的数据,进行渲染
+            currentPage = page;
+    
+            //调用render()重新渲染
+            render();
+        
+                
+            }
+          })
+
+        }
+      })  
     }
+  })
+
+        
+        
+    
 
 
-   })
-})
